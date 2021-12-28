0274e76b4f1491f2da52e0e55de1275dc1611a57
testsolo8 <testsolo8@gmail.com>
2021-12-28 15:39:57 +0300

    +

--- old/test_fixture2.py
+++ old/test_fixture2.py
@@ -20,3 +20,5 @@ class TestMainPage1():
     def test_guest_should_see_basket_link_on_the_main_page(self, browser):
         browser.get(link)
         browser.find_element_by_css_selector(".basket-mini .btn-group > a")
+
+        pytest -m "smoke or regression" --browser_name=firefox test_login.py
